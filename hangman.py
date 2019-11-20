cf17540338b6e3c658e1d08d7734c8f3797905b3
Jonathan-Talvacchio <jonathan.talvacchio@gmail.com>
2019-11-19 19:14:47 -0500

    added single character support

--- hangman.py
+++ hangman.py
@@ -27,7 +27,7 @@ def secret_display(secret_list, tries, used_letters):
 
 
 def game_main(default_word="mississippi"):
-    # init game
+    # --init game--
     clear_term()
     currect_count = 0
     tries = 0
@@ -35,13 +35,13 @@ def game_main(default_word="mississippi"):
     secret_list = list()
     used_letters = list()
 
-    # create secret word display
+    # --create secret word display--
     for letter in secret_word:
         if re.search("[^a-zA-Z-]", letter):
             secret_list.append(letter)
             currect_count += 1
         elif letter == "-":
-            # special case for - character
+            # --special case for - character--
             secret_list.append("(-)")
             currect_count += 1
         else:
@@ -50,17 +50,17 @@ def game_main(default_word="mississippi"):
     secret_display(secret_list, tries, used_letters)
 
     while True:
-        # game loop start
+        # --game loop start--
         usr_input = input("Type a letter: ")
         usr_input = usr_input.lower()
         usr_input = usr_input.replace(" ", "")
 
-        # check for win
+        # --check for win--
         if usr_input == secret_word:
             secret_list = usr_input
 
-        # check user input
-        # if user input contains anything but a-z & A-Z
+        # --check user input--
+        # --if user input contains anything but a-z & A-Z--
         elif re.search("[^a-zA-Z]", usr_input) or len(usr_input) > 1:
             clear_term()
             secret_display(secret_list, tries, used_letters)
@@ -73,20 +73,24 @@ def game_main(default_word="mississippi"):
             print("No input found")
             continue  # end loop here
 
-        # if letter NOT already entered
+        # --if letter NOT already entered--
         if usr_input not in used_letters:
-            # if letter is NOT part of secret word
+            # --if letter is NOT part of secret word--
             if usr_input not in secret_word:
                 used_letters.append(usr_input)
                 tries += 1
             else:
                 letter_index = 0
                 for letter in secret_word:
-                    # if user input matches secret word letter
-                    if usr_input == secret_word[letter_index]:
-                        secret_list[letter_index] = usr_input
+                    # --if user input matches secret word letter--
+                    if usr_input == letter:
+                        # --if secret word is more then one character--
+                        if len(secret_word) > 1:
+                            secret_list[letter_index] = letter
+                        else:
+                            secret_list = letter
                         currect_count += 1
-                        #used_letters.append(usr_input)
+                        # used_letters.append(usr_input)
 
                     letter_index += 1
 
@@ -97,7 +101,7 @@ def game_main(default_word="mississippi"):
             secret_display(secret_list, tries, used_letters)
             print(f"{usr_input.upper()}: was already entered")
 
-        # game win
+        # --game win--
         if "".join(secret_list) == secret_word:
             clear_term()
             print("YOU WIN!!")
@@ -105,7 +109,7 @@ def game_main(default_word="mississippi"):
             print(f"{_secret_display}: was the secret word or phrase")
             print(f"It only took you {tries} tries!")
 
-            # game restart
+            # --game restart--
             usr_input = input("\nPlay again? [Y/n]: ")
             usr_input = usr_input.lower()
             if re.search("[n]", usr_input):
